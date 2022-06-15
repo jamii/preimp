aa00e2c823f0ac841f6f5a2f101553334afbf60f
Jamie Brandon <jamie@scattered-thoughts.net>
2022-06-14 19:20:25 -0700

    Add a PUT endpoint for setting the value of a cell

--- src/preimp/core.clj
+++ src/preimp/core.clj
@@ -6,7 +6,8 @@
    [ring.middleware.file :refer [wrap-file]]
    [ring.middleware.resource :refer [wrap-resource]]
    preimp.state
-   [next.jdbc :as jdbc]))
+   [next.jdbc :as jdbc]
+   [clojure.data.json :as json]))
 
 ;; --- state ---
 
@@ -39,19 +40,36 @@
     (catch Exception error
       (prn [:ws-send-error ws error]))))
 
-(defn recv-ops [recv-ws msg-str]
-  (let [msg (clojure.edn/read-string {:readers preimp.state/readers} msg-str)
-        old-ops (@state :ops)
-        novel-ops (clojure.set/difference (:ops msg) old-ops)]
+(defn recv-ops [new-ops]
+  (let [old-ops (@state :ops)
+        novel-ops (clojure.set/difference new-ops old-ops)]
     (write-ops novel-ops)
+    (swap! state update-in [:ops] clojure.set/union new-ops)
+    old-ops))
+
+(defn recv-ops-from-ws [recv-ws msg-str]
+  (let [msg (clojure.edn/read-string {:readers preimp.state/readers} msg-str)
+        old-ops (recv-ops (:ops msg))]
     (swap! state assoc-in [:client->websocket (:client msg)] recv-ws)
-    (swap! state update-in [:ops] clojure.set/union (:ops msg))
     (when (not= old-ops (:ops msg))
       (send-ops recv-ws))
     (when (not= old-ops (@state :ops))
       (doseq [[client other-ws] (@state :client->websocket)]
         (when (not= client (:client msg))
           (send-ops other-ws))))))
+
+(defn recv-ops-from-put [input-stream]
+  (let [msg (json/read-str (slurp input-stream))
+        op (preimp.state/->AssocOp
+            (preimp.state/next-version (@state :ops))
+            (java.util.UUID/randomUUID)
+            (java.util.UUID/fromString (get msg "cell-id"))
+            :code
+            (get msg "value"))
+        old-ops (recv-ops #{op})]
+    (when (not= old-ops (@state :ops))
+      (doseq [[client other-ws] (@state :client->websocket)]
+        (send-ops other-ws)))))
 
 ;; --- change polling ---
 
@@ -67,7 +85,6 @@
 
 (defn read-ops-if-changed []
   (when (db-changed?)
-    (prn :refreshing)
     (read-ops)
     ;; TODO need to centralize decisions about when to send updates
     (doseq [[client other-ws] (@state :client->websocket)]
@@ -97,15 +114,23 @@
                (prn [:ws-close status-code reason]))
    :on-text (fn [ws text]
               (read-ops-if-changed)
-              (recv-ops ws text))
+              (recv-ops-from-ws ws text))
    :on-bytes (fn [ws bytes offset len])
    :on-ping (fn [ws bytebuffer])
    :on-pong (fn [ws bytebuffer]
               (read-ops-if-changed))})
 
 (defn handler [request]
-  (if (jetty/ws-upgrade-request? request)
+  (cond
+    (jetty/ws-upgrade-request? request)
     (jetty/ws-upgrade-response websocket-handler)
+
+    (= :put (:request-method request))
+    (do
+      (recv-ops-from-put (:body request))
+      {:status 200})
+
+    :else
     {:status 200
      :headers {"Content-Type" "text/html"}
      :body page}))
